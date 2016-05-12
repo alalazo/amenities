3b00e359188f77556de9637c69ef31ed5a17f6df
alalazo <massimiliano.culpo@googlemail.com>
2016-05-12 16:46:11 +0200

    cli options

--- nqueens/base/main.cxx
+++ nqueens/base/main.cxx
@@ -1,11 +1,14 @@
+#include <boost/program_options.hpp>
+
 #include <algorithm>
 #include <iostream>
 #include <numeric>
 #include <vector>
-#include <tuple>
 
 using Index = unsigned short int;
 using Placement = std::vector<Index>;
+
+namespace po = boost::program_options;
 
 bool is_valid_placement(const Placement& placement) {
     for (auto ii = 0u; ii < placement.size(); ++ii) {
@@ -24,15 +27,38 @@ bool is_valid_placement(const Placement& placement) {
     return true;
 }
 
-int main() {
-    auto v = Placement(12);
-    std::iota(v.begin(), v.end(), 0);
-    auto count = 0;
-    do {
-        if (is_valid_placement(v)) {
-            count++;
+int main(int argc, char** argv) {
+    // Handle input
+    try {
+        auto n = 0;
+        auto description = po::options_description("Allowed options:");
+        description.add_options()
+                ("help,h", "prints this nice help message")
+                ("size,n", po::value<int>(&n)->default_value(8), "size of the board")
+                ;
+
+        auto vm = po::variables_map();
+        po::store(po::parse_command_line(argc, argv, description), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << description << std::endl;
+            return 1;
         }
-    } while (std::next_permutation(v.begin(), v.end()));
-    std::cout << "Found " << count << " solution" << std::endl;
+
+        // Compute
+        auto v = Placement(n);
+        std::iota(v.begin(), v.end(), 0);
+        auto count = 0;
+        do {
+            if (is_valid_placement(v)) {
+                count++;
+            }
+        } while (std::next_permutation(v.begin(), v.end()));
+        std::cout << "Found " << count << " solution" << std::endl;
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
